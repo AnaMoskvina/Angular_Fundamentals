2a099827990162992a9e3fcff6b8553e3c6d54bc
Anastasiia Moskvina <anastasiia_moskvina@epam.com>
2022-02-12 18:19:00 +0300

    add dynamic fields to course form, in progress

--- courses-app/src/app/features/course/course-form/course-form.component.ts
+++ courses-app/src/app/features/course/course-form/course-form.component.ts
@@ -1,3 +1,4 @@
+import { ConditionalExpr } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators, FormArray } from '@angular/forms';
 import { nameValidator } from 'src/app/shared/directives/name-validator/name-validator.directive';
@@ -17,22 +18,29 @@ export class CourseFormComponent implements OnInit {
       'title': new FormControl(null, Validators.required),
       'description': new FormControl(null, Validators.required),
       'duration':  new FormControl(null, [Validators.required, Validators.min(0)]),
-      'authorName': new FormControl(null, [Validators.required, nameValidator()]),
+      'authorName': new FormControl(null, [nameValidator()]),
       'authorsList': new FormArray([])
     });
   }
 
   onFormSubmit() {
-    console.log(this.courseForm);
+    // console.log(this.courseForm);
   }
 
-  onCreateAuthor() {
-    const control = new FormControl(null, [nameValidator()]);
-    console.log(control);
+  onCreateAuthor(value: string) {
+    const control = new FormControl({value, disabled: true}, Validators.required);
     (<FormArray>this.courseForm.get('authorsList')).push(control);
   }
 
-  get authorsList() {
-    return this.courseForm.controls["authorsList"] as FormArray;
+  onRemoveAuthor(id: any) {
+    const control = (<FormArray>this.courseForm.get('authorsList')).controls;
+    console.log(id, control)
+    // this.courses = this.courses.filter(course => course.title !== currentCourse.title);
+    // (<FormArray>this.courseForm.get('authorsList')).filter(control => control !== author.id);
   }
+
+  getAuthorsList(courseForm: any) {
+    return courseForm.get('authorsList').controls;
+  }
+
 }
